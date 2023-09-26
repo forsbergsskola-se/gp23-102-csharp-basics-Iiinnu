fd2b0435b432dfa043bd01725548703d7cecf71b
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2023-09-26 11:19:24 +0200

    Completed P05_10Parameter (Fibonacci)

---
+++ P05_10Paramater/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int Fibonacci(int n)
+{
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+
+    int first = 0;
+    int second = 1;
+
+    NextIteration:
+    var current = first + second;
+    first = second;
+    second = current;
+    n--;
+    if (n > 1)
+    {
+        goto NextIteration;
+    }
+    return current;
+}
+ 
