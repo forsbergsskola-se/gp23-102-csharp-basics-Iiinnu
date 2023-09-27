19f777f6fbd6e6dab5e985ccd5506c1e11126aab
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2023-09-27 13:30:05 +0200

    Types exercise

---
+++ P06types/Program.cs
@@ -0,0 +1,27 @@
+﻿{
+    string outPutString = ("Hello World");
+    
+    Console.WriteLine(outPutString);
+
+}
+
+{
+    float decimalValue = 0.3f;
+
+    Console.WriteLine(decimalValue);
+}
+
+{
+
+    bool isAwesome = (true);
+    
+    Console.WriteLine(isAwesome);
+}
+
+{
+    byte hexByte = (0xa7);
+
+    Console.WriteLine(hexByte);
+   
+
+}
