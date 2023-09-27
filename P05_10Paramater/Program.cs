d60e2dbefdcba2ffe685d45dbccb86e538b181cf
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2023-09-27 11:13:06 +0200

    Variable exercise

--- P05_10Paramater/Program.cs
+++ P05_10Paramater/Program.cs
@@ -1,23 +1,45 @@
-﻿
-int Fibonacci(int n)
-{
-    if (n == 0) return 0;
-    if (n == 1) return 1;
+﻿ {
+     int Fibonacci(int n)
+     {
+         //tells the code if its 0 or 1 just give the result 0 or 1
+         if (n == 0) return 0;
+         if (n == 1) return 1;
 
-    int first = 0;
-    int second = 1;
+         int first = 0;
+         int second = 1;
 
-    NextIteration:
-    var current = first + second;
-    first = second;
-    second = current;
-    n--;
-    if (n > 1)
+
+         NextIteration:
+         var current = first + second;
+         first = second;
+         second = current;
+         n--;
+         if (n > 1)
+         {
+             goto NextIteration;
+         }
+
+         return current;
+     }
+
+
+     Console.WriteLine(Fibonacci(4));
+
+ }
+ {
+    int Fibonacci(int n)
     {
-        goto NextIteration;
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int first = Fibonacci(n - 2);
+        int second = Fibonacci(n - 1);
+
+        return first + second;
     }
-    return current;
+
+    int result = Fibonacci(4);
+    Console.WriteLine(result);
+    
+    
 }
- 
- 
- Console.WriteLine(Fibonacci(4));
---
+++ P5Variables/Program.cs
@@ -0,0 +1,20 @@
+﻿
+//When you use the "" it is a quote, it writes whats in the Quote.
+//Here we want to use the Veriable Kingposition
+//Thet contains the Value d1 instead
+//To do that you write it without the "".
+
+//We start with declaring a variable named kingposition 
+
+    //Place of the king
+        string Kingposition = "d1"; //Declaring the variable, Type,Name (assigment operator = VALUE);
+        Console.WriteLine(Kingposition);
+
+//Move the king
+
+    Kingposition = "d2";
+    Console.WriteLine(Kingposition); 
+//Here we pass the value on the Variable to the function Writeline.
+
+
+
