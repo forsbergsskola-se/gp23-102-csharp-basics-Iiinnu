1c0415e3f1117c1742ee3fda74e24392e55390f9
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2023-10-08 14:51:42 +0200

    Operator Exercise

--- P09_07Product/Program.cs
+++ P09_07Product/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Give us a number");
+
+int first = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Give us a second number");
+
+int second = int.Parse(Console.ReadLine());
+
+int sum = (first * second);
+
+Console.WriteLine($"That is {sum} multiplied");
---
+++ P09_10SecondsToMinutes/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Give me a number of seconds");
+
+int totalSeconds = int.Parse(Console.ReadLine());
+
+int minutes = totalSeconds / 60; //111 divided in 60 would just be 1
+
+int seconds = totalSeconds % 60; // 111/60 = 1 + 51. % is what remaning until 60. 
+
+Console.WriteLine($"Seconds: {seconds}");
+Console.WriteLine($"Minutes: {minutes}");
+
+
+
