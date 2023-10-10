ef377ee35df6dd50e929a3e898422c2b8e287a32
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2023-10-10 16:00:12 +0200

    If/Else exercise

---
+++ P11IfElse/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Threading.Channels;
+
+Console.WriteLine("Give me your age");
+
+int age = int.Parse(Console.ReadLine());
+
+bool isAdult = age >= 18;
+
+if (isAdult)
+{
+    Console.WriteLine("You can now start watching resident evil 3");
+    Console.WriteLine();
+}
+
+else
+{
+    Console.WriteLine("You are not old enough, sorry");
+    Console.WriteLine($"Come back in {18-age} years! ");
+}
+   Console.WriteLine("You can now watch another movie!"); 
