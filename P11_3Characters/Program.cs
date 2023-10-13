4a8c6f0fd9ec3e0468267ad57df918894ac1be92
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2023-10-13 15:37:43 +0200

    If/else exercise

---
+++ P11_3Characters/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Please enter a character");
+
+char character = char.Parse(Console.ReadLine());
+char lowerCaseCharacter = char.ToLower(character);
+
+if (char.IsDigit(character))
+
+     Console.WriteLine("The character you added is a digit");
+
+else if (lowerCaseCharacter == 'a' || lowerCaseCharacter == 'e' || lowerCaseCharacter == 'i' ||
+         lowerCaseCharacter == 'o' || lowerCaseCharacter == 'u')
+     Console.WriteLine("The character you added is a vowel");
+
+else
+
+     Console.WriteLine("The character you added is a constant"); 
+
+
