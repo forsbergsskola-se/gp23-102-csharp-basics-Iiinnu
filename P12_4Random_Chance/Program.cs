44533c7d930b90c1aa5e22f5736947482c5869d8
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2023-11-03 15:32:38 +0100

    Random chance exercise

---
+++ P12_4Random_Chance/Program.cs
@@ -0,0 +1,17 @@
+﻿
+if (Random.Shared.NextDouble() < 0.1)
+{
+    Console.WriteLine("You only had a 10% chance, but you were successful!");
+}
+else
+{
+    Console.WriteLine("You were not successful.");
+}
+
+
+
+
+
+
+
+
