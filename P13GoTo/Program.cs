fea5a02419b46d9ed72b698f72dd46fb18568163
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2023-11-03 15:58:18 +0100

    Goto exercise

---
+++ P13GoTo/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Random random=new Random();
+int myNumber = random.Next(0,100);
+
+Console.WriteLine("I have picked a number (1-100). It's your turn to guess it!");
+
+askfornumber:
+string userInput = Console.ReadLine();
+int userNumber= int.Parse(userInput);
+
+
+
+if (myNumber < userNumber)
+{
+    Console.WriteLine("Guess on a lower number");
+    goto askfornumber;
+}
+else if (myNumber > userNumber)
+{
+    Console.WriteLine("Guess Higher");
+    goto askfornumber;
+}
+else
+{
+    Console.WriteLine("You guessed right");
+}
