75dd0a0f0e242b500cb01c7cb2758cbd0083471c
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2023-11-03 15:16:04 +0100

    Random item exercise

---
+++ P12_3Random_Item/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] items = {"Sward","Shield","Bat","Armor","Potions"};
+
+Random random = new Random();
+
+int randomIndex = random.Next(items.Length);
+String playerItem = items[randomIndex];
+
+Console.WriteLine("Congratulations you have won:" +playerItem);
