da28d0c61b91f4a2e10cf722c313966b0962ff2d
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2023-11-03 14:55:23 +0100

    Random Password Exercise

---
+++ P12_2Random_Password/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Random random = new Random();
+
+int number1 = random.Next(9);
+int number2 = random.Next(9);
+int number3 = random.Next(9);
+int number4 = random.Next(9);
+int number5 = random.Next(9);
+int number6 = random.Next(9);
+
+Console.WriteLine("Your new password is:" + number1 +number2 +number3 + number4 + number5 + number6);
